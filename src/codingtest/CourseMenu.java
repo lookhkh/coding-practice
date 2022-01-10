d67090e090bc5b66831a21923baf22878dc6f81d
lookhkh <lookhkh37@gmail.com>
2022-01-10 19:12:24 +0900

    카카오

--- src/codingtest/CourseMenu.java
+++ src/codingtest/CourseMenu.java
@@ -1,10 +1,14 @@
 package codingtest;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
 public class CourseMenu {
 
 	public static void main(String[] args) {
 		Solution7 sol = new Solution7();
-		String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+		String[] orders = {"XYZ", "XWY", "WXA"};
 		int[] course = {2,3,4};
 		sol.solution(orders, course);
 	}
@@ -14,23 +18,55 @@ public class CourseMenu {
 
 class Solution7 {
 	
-	static int n; 
-	static boolean[] checked;
-
-	
+	static boolean[] check;
+	static HashMap<String, Integer> map = new HashMap<>();
+    static String[] answer;
+    static int[] results;
+    
     public String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
-     
-        checked = new boolean[orders[0].length()];
+        answer = new String[course.length];
+        results = new int[course.length];
+        check = new boolean[orders[0].length()];
+       
+       dfs(orders[0], 2, "",0);  		    		
+      
         
+        
+      
         
         return answer;
     }
     
-    private void dfs(int L) {
+    private void dfs(String orders, int level, String result,int idx) {
+    	    	
+    	if(idx==orders.length()) {
+    		for(int i=0; i<check.length; i++) {
+    			if(check[i]==true) {
+    				result+=orders.charAt(i);
+    			}    			
+    		}
+    		System.out.println("°á°ú : "+result);
+    		System.out.println(idx);
+
+			
+				if(map.containsKey(result)) {
+					map.put(result, map.get(result)+1);
+				}else {
+					map.put(result, 1);
+				}
+				
+			return;
+    	}else if(idx>orders.length()) {
+    		return;
+    	}
     	
-    	
-    		
+    	check[idx] = true;
+    	result+=orders.charAt(idx);
+    	dfs(orders,level,result,idx+1);
+    	check[idx] = false;
+    	result = result.substring(0, result.lastIndexOf(orders.charAt(idx))-1);
+    	System.out.println(result);
+    	dfs(orders,level,result,idx+1);	
     	}
 }
 
